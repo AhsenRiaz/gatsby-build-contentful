a7bf6276fa1f9b0d2ba747696dcd7af70463e59a
AhsenRiaz <ahsenriaz2001@gmail.com>
2021-07-07 15:33:57 +0500

    Updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,17 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
 module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            resolve: "gatsby-source-contentful",
+            options : {
+                spaceId: process.env.CONTENTFUL_SPACE_ID ,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            },
+        },
     ]
 }
